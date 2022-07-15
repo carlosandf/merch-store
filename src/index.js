723107f4f94059dd9dcf5744c4f24bc2de1f18cf
carlosandf <carlos.andres.ff2912@gmail.com>
2022-07-15 02:34:16 -0500

    Init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@components/App';
+
+const app = document.getElementById('app')
+
+const root = ReactDOM.createRoot(app);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

2b8ca402f858bfc0f9c874e03706876d4f87f86a
carlosandf <carlos.andres.ff2912@gmail.com>
2022-07-16 15:48:46 -0500

    sass config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPluin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +13,7 @@ module.exports = {
     alias: {
       '@components': path.resolve(__dirname, './src/components/'),
       '@styles': path.resolve(__dirname, './src/styles/'),
+      '@stylesComponents': path.resolve(__dirname, './src/styles/components'),
     },
   },
   module: {
@@ -28,14 +30,27 @@ module.exports = {
         use: {
           loader: 'html-loader'
         }
-      }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPluin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPluin({
+      filename: 'assets/[name].css'
+    }),
   ],
   devServer: {
     static: {
